a968fa98314720bb4442027594b15074b469e5a9
Daniel Lyons <fusion@storytotell.org>
2017-07-22 15:55:30 -0600

    making a trivial change

--- fabfile.py
+++ fabfile.py
@@ -2,34 +2,58 @@ from fabric.api import *
 from fabtools import require
 import fabtools
 
-@task
-def setup():
+@task(default=True)
+def deploy():
+    execute(system_dependencies)
+    execute(setup_health)
+    execute(start_serving)
 
+@task
+@hosts(['root@health2.7gf.org'])
+def system_dependencies():
     # Require some Debian/Ubuntu packages
-    # require.deb.packages([
-    #     'imagemagick',
-    #     'libxml2-dev',
-    # ])
+    require.deb.packages([
+        'python3',
+        'nginx-full',
+        'python3-dev',
+        'python3-pip',
+        'python3-venv'
+    ])
+
+    # let's make a user for our app
+    require.user('health')
+
+@task
+@hosts(['health@health2.7gf.org'])
+def setup_health():
+    # copy this up there
+    require.git.working_copy('https://github.com/fusiongyro/health2', 'health')
+
+    run("python3 -m venv ~/venv")
 
     # Require a Python package
-    with fabtools.python.virtualenv('venv'):
-        require.python.package('pyramid')
+    with prefix('source ~/venv/bin/activate'):
+        with cd('health'):
+            run('python setup.py develop')
 
+@task
+@hosts(['root@health2.7gf.org'])
+def start_serving():
     # Require a PostgreSQL server
     # require.postgres.server()
     # require.postgres.user('myuser', 's3cr3tp4ssw0rd')
     # require.postgres.database('myappsdb', 'myuser')
 
     # Require a supervisor process for our app
-    require.supervisor.process('hello',
-                               command='hello',
-                               directory='health')
+    #require.supervisor.process('health',
+    #                           command='/home/health/venv/bin/pserve /home/health/health/production.ini',
+    #                           directory='/home/health/health',
+    #                           user='health')
 
     # Require an nginx server proxying to our app
-    # require.nginx.proxied_site('example.com',
-    #                            docroot='/home/myuser/env/myapp/myapp/public',
-    #                            proxy_url='http://127.0.0.1:8888'
-    #                            )
+    require.nginx.proxied_site('health2.7gf.org',
+                               docroot='/home/health/health/static',
+                               proxy_url='http://127.0.0.1:6543')
 
     # Setup a daily cron task
     # fabtools.cron.add_daily('maintenance', 'myuser', 'my_script.py')
